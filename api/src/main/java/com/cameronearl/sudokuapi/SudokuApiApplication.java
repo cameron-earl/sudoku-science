fc3777f872c25314e031d8a2ec0e8e7266b2b548
Cameron Earl <developer.cameron@gmail.com>
2018-01-24 15:56:52 -0700

    Initial commit

---
+++ api/src/main/java/com/cameronearl/sudokuapi/SudokuApiApplication.java
@@ -0,0 +1,12 @@
+package com.cameronearl.sudokuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokuApiApplication.class, args);
+	}
+}
